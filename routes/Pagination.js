6485ac9c72e0b68c1808cff6273f1dad0d7acaaf
Jordan <jr.goedbloed@gmail.com>
2023-12-15 12:46:15 +0100

    added pagination functions but could not make it work yet

--- routes/Pagination.js
+++ routes/Pagination.js
@@ -1,270 +1,10 @@
-// import express from "express";
-// import Kitty from "../models/Kitten.js";
-// import "dotenv/config";
-
-// const routes = express.Router();
-
-// Pagination Functions
-// function currentItems(total, start, limit) {
-//     if (!limit || !start) {
-//         return total;
-//     }
-//     return Math.min(total - start, limit);
-// }
-
-// function numberOfPages(total, start, limit) {
-//     if (!limit || !start) {
-//         return 1;
-//     }
-//     return Math.ceil(total / limit);
-// }
-
-// function currentPage(start, limit) {
-//     if (!limit || !start) {
-//         return 1;
-//     }
-//     return Math.floor(start / limit) + 1;
-// }
-
-// function firstPageItem() {
-//     return 1;
-// }
-
-// function lastPageItem(total, start, limit) {
-//     if (!limit || !start) {
-//         return total;
-//     }
-//     if (limit == 1) {
-//         return total;
-//     }
-//     return (numberOfPages(total, start, limit) - 1) * limit;
-// }
-
-// function previousPageItem(start, limit) {
-//     if (!limit || !start) {
-//         return null;
-//     }
-//     return Math.max(start - limit, firstPageItem());
-// }
-
-// function nextPageItem(total, start, limit) {
-//     if (!limit || !start) {
-//         return null;
-//     }
-//     return Math.min(start + limit, lastPageItem(total, start, limit));
-// }
-
-// function getFirstQueryString(total, start, limit) {
-//     let queryString = "";
-//     if (limit && start) {
-//         queryString = `?start=${firstPageItem(total, start, limit)}&limit=${limit}`;
-//     }
-//     return queryString;
-// }
-
-// function getLastQueryString(total, start, limit) {
-//     let queryString = "";
-//     if (limit && start) {
-//         queryString = `?start=${lastPageItem(total, start, limit)}&limit=${limit}`;
-//     }
-//     return queryString;
-// }
-
-// function getPreviousQueryString(start, limit) {
-//     let queryString = "";
-//     if (limit && start) {
-//         let previousStart = previousPageItem(start, limit);
-//         if (previousStart !== null) {
-//             queryString = `?start=${previousStart}&limit=${limit}`;
-//         }
-//     }
-//     return queryString;
-// }
-
-// function getNextQueryString(total, start, limit) {
-//     let queryString = "";
-//     if (limit && start) {
-//         let nextStart = nextPageItem(total, start, limit);
-//         if (nextStart !== null) {
-//             queryString = `?start=${nextStart}&limit=${limit}`;
-//         }
-//     }
-//     return queryString;
-// }
-
-// function itemToPageNumber(start, limit, itemNumber) {
-//     if (!limit || !start) {
-//         return 1;
-//     }
-//     return Math.ceil(itemNumber / limit);
-// }
-
-// function createPagination(total, start, limit) {
-//     let pagination = {
-//         currentPage: currentPage(start, limit),
-//         numberOfPages: numberOfPages(total, start, limit),
-//         currentItems: currentItems(total, start, limit),
-//         firstPageQueryString: getFirstQueryString(total, start, limit),
-//         lastPageQueryString: getLastQueryString(total, start, limit),
-//         previousPageQueryString: getPreviousQueryString(start, limit),
-//         nextPageQueryString: getNextQueryString(total, start, limit),
-//         itemToPageNumber: itemToPageNumber(start, limit),
-//     };
-//     return pagination;
-// }
-
-
-// routes.use(express.json());
-// // routes.use(express.urlencoded());
-
-
-// routes.options('/', function (req, res, next) {
-//     res.header('Allow', 'GET, POST, OPTIONS'); // Voeg hier de juiste methoden toe die zijn toegestaan
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Zelfde methoden als Allow-header
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Accept, Authorization, Origin');
-//     res.header('Access-Control-Allow-Origin', '*');
-  
-//     if (req.method === 'OPTIONS') {
-//       return res.sendStatus(200);
-//     }
-//     next();
-//   });
-  
-//   routes.options('/:id', function (req, res, next) {
-//     res.header('Allow', 'GET, POST, DELETE, OPTIONS'); // Voeg hier de juiste methoden toe die zijn toegestaan
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS'); // Zelfde methoden als Allow-header
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Accept, Authorization, Origin');
-//     res.header('Access-Control-Allow-Origin', '*');
-  
-//     if (req.method === 'OPTIONS') {
-//       return res.sendStatus(200);
-//     }
-//     next();
-//   });
-
-
-
-
-//   routes.get("/kitty", async (req, res) => {
-//     try {
-//         const acceptHeader = req.headers['accept'];
-
-//         if (acceptHeader === 'application/json') {
-//             // Verzoek voor JSON-formaat
-//             // Genereer JSON-respons
-//             // Je JSON-responscode hier
-//         } else if (acceptHeader === 'application/xml') {
-//             // Verzoek voor XML-formaat
-//             // Genereer XML-respons
-//             // Je XML-responscode hier
-//         } else {
-//             // Geen ondersteund formaat gevraagd, stuur 406 Not Acceptable-status
-//             res.status(406).send('Not Acceptable');
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send("Internal Server Error");
-//     }
-// });
-
-
-
-// routes.get("/", async (req, res) => {
-//     try {
-//       // Je ontvangt de Accept-header van het verzoek
-//       const acceptHeader = req.headers['accept'];
-  
-//       // Controleer de waarde van de Accept-header
-//       if (acceptHeader === 'application/json') {
-//         // Verzoek voor JSON-formaat
-//         // Genereer JSON-respons
-//         res.json({ message: 'Response in JSON format' });
-//       } else if (acceptHeader === 'application/xml') {
-//         // Verzoek voor XML-formaat
-//         // Genereer XML-respons
-//         res.type('application/xml').send('<message>Response in XML format</message>');
-//       } else {
-//         // Geen ondersteund formaat gevraagd, stuur 406 Not Acceptable-status
-//         res.status(406).send('Not Acceptable');
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send("Internal Server Error");
-//     }
-//   });
-
-
-// // GET all kitty's with pagination
-
-
-// routes.get("/", async (req, res) => {
-//     try {
-//         let start = isNaN(parseInt(req.query.start)) ? undefined : parseInt(req.query.start);
-//         let limit = isNaN(parseInt(req.query.limit)) ? undefined : parseInt(req.query.limit);
-
-//         const kitties = await Kitty.find()
-//             .skip((currentPage(start, limit) - 1) * limit)
-//             .limit(limit);
-
-//         const totalKitties = await Kitty.countDocuments();
-//         const pagination = createPagination(totalKitties, start, limit);
-
-//         const kittiesCollection = {
-//             items: kitties.map((kitty) => {
-//                 const kittyJson = kitty.toJSON();
-//                 return {
-//                     ...kittyJson,
-//                     _links: {
-//                         self: {
-//                             href: `${process.env.URI}/kitty/${kittyJson._id}`,
-//                         },
-//                     },
-//                 };
-//             }),
-//             _links: {
-//                 self: {
-//                     href: `${process.env.URI}/kitty/`,
-//                 },
-//                 // Voeg andere links toe indien nodig
-//             },
-//             pagination: {
-//                 currentPage: pagination.currentPage,
-//                 currentItems: pagination.currentItems,
-//                 totalPages: pagination.numberOfPages,
-//                 totalItems: totalKitties,
-//                 _links: {
-//                     first: {
-//                         href: process.env.URI + pagination.firstPageQueryString,
-//                     },
-//                     last: {
-//                         href: process.env.URI + pagination.lastPageQueryString,
-//                     },
-//                     previous: {
-//                         href: process.env.URI + pagination.previousPageQueryString,
-//                     },
-//                     next: {
-//                         href: process.env.URI + pagination.nextPageQueryString,
-//                     },
-//                 },
-//             },
-//         };
-
-//         res.json(kittiesCollection);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send("Internal Server Error");
-//     }
-// });
-
-// export default routes
-
 
 import express from "express";
 import Kitty from "../models/Kitten.js";
 import "dotenv/config";
 import { faker } from '@faker-js/faker';
 
-// functions for pagination
+// functions for pagination die ik niet werkende heb gekregen.
 function currentItems(total, start, limit) {
     if (!limit || !start) {
         return total;
@@ -276,7 +16,7 @@ function numberOfPages(total, start, limit) {
     if (!limit || !start) {
         return 1;
     }
-    return Math.ceil(itemsToShow / limit);
+    return Math.ceil(total / limit);
 }
 
 
@@ -301,6 +41,9 @@ function lastPageItem(total, start, limit) {
     return Math.min((currentPage(total, start, limit) - 1) * limit + limit, total);
 }
 
+
+
+
 function previousPageItem(start, limit) {
     if (!limit || !start) {
         return null;
@@ -313,6 +56,14 @@ function nextPageItem(total, start, limit) {
         return null;
     }
     return Math.min(start + limit, lastPageItem(total, start, limit));
+}
+
+function getFirstQueryString(total, start, limit) {
+    let queryString = "";
+    if (limit && start) {
+        queryString = `?start=${firstPageItem(total, start, limit)}&limit=${limit}`;
+    }
+    return queryString;
 }
 
 function getLastQueryString(total, start, limit) {
@@ -343,6 +94,27 @@ function getNextQueryString(total, start, limit) {
         }
     }
     return queryString;
+}
+
+function itemToPageNumber(start, limit, itemNumber) {
+    if (!limit || !start) {
+        return 1;
+    }
+    return Math.ceil(itemNumber / limit);
+}
+
+function createPagination(total, start, limit) {
+    let pagination = {
+        currentPage: currentPage(start, limit),
+        numberOfPages: numberOfPages(total, start, limit),
+        currentItems: currentItems(total, start, limit),
+        firstPageQueryString: getFirstQueryString(total, start, limit),
+        lastPageQueryString: getLastQueryString(total, start, limit),
+        previousPageQueryString: getPreviousQueryString(start, limit),
+        nextPageQueryString: getNextQueryString(total, start, limit),
+        itemToPageNumber: itemToPageNumber(start, limit),
+    };
+    return pagination;
 }
 
 const routes = express.Router();
@@ -396,8 +168,12 @@ routes.use((req, res, next) => {
     res.locals.acceptedTypes = acceptedTypes;
     next();
 })
-    
 
+
+
+
+
+  
 routes.get("/", async (req, res) => {
     try {
         const acceptHeader = req.headers['accept'];
@@ -467,6 +243,9 @@ routes.get("/", async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 });
+
+
+
 
 
 
